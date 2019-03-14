d8a36447ab5fc466d4d708071fe55cf066cc56a0
Florian Heller <florian.heller@uhasselt.be>
2019-03-14 22:28:11 +0100

    Initial commit, yaw is available in some form

---
+++ MotionHeadset/MotionHeadset.ino
@@ -0,0 +1,108 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+#define UUID_SERVICE_IMU "7ca251df-137b-41b2-9169-1c0215bea6de"
+#define UUID_CHAR_COMBOHPR "919d5add-298f-4431-acf9-9f67275f1455"
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  bno.setExtCrystalUse(true);
+
+  // Create the BLE Device
+  BLEDevice::init("MotionHeadset");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(UUID_SERVICE_IMU);
+
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      UUID_CHAR_COMBOHPR,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+                    
+  // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
+  // Create a BLE Descriptor                  
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(UUID_SERVICE_IMU);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// notify changed value
+    if (deviceConnected) {
+        imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+        // TODO: Check if we can hand over the entire heading vector like this
+        //pCharacteristic->setValue((uint8_t*)&euler, 6);
+        uint16_t x = euler.x();
+        pCharacteristic->setValue(x);
+        pCharacteristic->notify();
+        delay(10); // bluetooth stack will go into congestion, if too many packets are sent, in 6 hours test i was able to go as low as 3ms
+    }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+}
